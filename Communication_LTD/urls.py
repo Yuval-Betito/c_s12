f92b79286646338249e813281c486f43374289ee
Yuval-Betito <yuval36811@gmail.com>
2024-12-30 23:10:33 +0200

    Update urls.py

--- Communication_LTD/urls.py
+++ Communication_LTD/urls.py
@@ -1,3 +1,5 @@
+# Communication_LTD/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views  # Import this to fix the issue
@@ -10,10 +12,6 @@ urlpatterns = [
     # נתיבים לשינוי סיסמה
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='users/password_change.html'), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
-
-    # נתיבים לשכחתי סיסמה ואיפוס סיסמה
-    path('forgot_password/', views.forgot_password, name='forgot_password'),
-    path('reset_password/', views.reset_password, name='reset_password'),
 
     # נתיב להתנתקות
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),  # התנתקות עם הפניה למסך כניסה
